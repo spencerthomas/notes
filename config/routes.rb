ca47d2afbbedb27817c38bb899d0f0e9829188b8
spencerthomas <tom@tomspencer.co>
2021-10-01 11:12:46 +1000

    updated version of ruby and rails to 3 and 6 respectivley. remigrated database

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  namespace :admin do
-      resources :notes
-
-      root to: "notes#index"
-    end
-  resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :notes
 
-  root "notes#index"
-  
+    root to: "notes#index"
+  end
+resources :notes
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+root "notes#index"
+
 end
